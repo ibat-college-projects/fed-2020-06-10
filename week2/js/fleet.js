786b0dbfe0d831871eb4f63614f6195b48bd047e
John Rowley <outsidethebox@eircom.net>
2020-06-17 21:07:00 +0100

    Week 2: Map , Filter, Find

---
+++ week2/js/fleet.js
@@ -0,0 +1,68 @@
+var fleet = [
+    {
+        'mf': 'Volkswagen',
+        'model': `Golf`,
+        'engine': '1.6',
+        'colour': 'Red',
+        'type': 'Hatchback'
+    },
+
+    {
+        'mf': 'Volkswagen',
+        'model': `Tiguan`,
+        'engine': '1.4',
+        'colour': 'Blue',
+        'type': 'Hatchback'
+    },
+
+    
+    {
+        'mf': 'Volkswagen',
+        'model': `Passat`,
+        'engine': '1.3',
+        'colour': 'Brown',
+        'type': 'Estate'
+    },
+
+    {
+        'mf': 'Nissan',
+        'model': `Navara`,
+        'engine': '2.5',
+        'colour': 'Grey',
+        'type': 'Pickup'
+    },
+
+    {
+        'mf': 'Ford',
+        'model': `Fiesta`,
+        'engine': '0.1',
+        'colour': 'Pink',
+        'type': 'Sedan'
+
+    }
+
+
+];
+
+var pilots = [
+    {
+      id: 2,
+      name: "Wedge Antilles",
+      faction: "Rebels",
+    },
+    {
+      id: 8,
+      name: "Ciena Ree",
+      faction: "Empire",
+    },
+    {
+      id: 40,
+      name: "Iden Versio",
+      faction: "Empire",
+    },
+    {
+      id: 66,
+      name: "Thane Kyrell",
+      faction: "Rebels",
+    }
+  ];
