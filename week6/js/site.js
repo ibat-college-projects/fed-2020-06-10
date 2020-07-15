e44ec31cdc66823e4be91fa4a45d56b154e78c14
John Rowley <outsidethebox@eircom.net>
2020-07-15 20:39:33 +0100

    adding interaction with radio buttons

--- week6/js/site.js
+++ week6/js/site.js
@@ -2,10 +2,52 @@ $(function () {
 
     console.log('jquery is loaded');
 
-    initSelect()
+
+    hideToppings();
+    initToppingChoice();
+    initMeatSelect()
+    initVegSelect()
 })
 
-function initSelect() {
+function initToppingChoice() {
+
+    $("input[name='toppingChoice']").on('click',function () {
+        var choice = $(this).val();
+
+        console.log(`You have chosen ${choice}`);
+        showPizzaToppingChoice(choice);
+    });
+}
+
+function showPizzaToppingChoice(choice) {
+
+    $('#listOfMeatToppings').hide();
+    $('#listOfVegToppings').hide();
+
+    switch (choice) {
+        case 'meat':
+            $('#listOfMeatToppings').show();
+            break;
+        case 'vegetarian':
+            $('#listOfVegToppings').show();
+            break;
+        case 'both':
+            $('#listOfMeatToppings').show();
+            $('#listOfVegToppings').show();
+            break;
+        default: break;
+
+    }
+}
+
+
+function hideToppings() {
+
+    $('#listOfMeatToppings').hide();
+    $('#listOfVegToppings').hide();
+}
+
+function initMeatSelect() {
 
 
     $('#SelectAllMeat').on('click', function () {
@@ -35,6 +77,47 @@ function initSelect() {
 
             if (countMeatsNotSelected == 0) {
                 $('#SelectAllMeat').prop('checked', true);
+            }
+        }
+
+
+
+    })
+
+
+
+}
+
+function initVegSelect() {
+
+
+    $('#SelectAllVegetarian').on('click', function () {
+        // console.log(`select all `);
+
+        const currentState = $(this).prop('checked');
+        $('.Vegetarian').prop('checked', currentState)
+
+
+    })
+
+
+    $('.Vegetarian').on('click', function () {
+
+        // const currentStateAll = $('#SelectAllMeat').prop('checked');
+        const currentState = $(this).prop('checked');
+
+        if (!currentState) {
+
+            $('#SelectAllVegetarian').prop('checked', false);
+        } else {
+
+            const countVegNotSelected = $("input[name='Vegetarian']:not(':checked')").length;
+
+            console.log(`Items not checked = ${countVegNotSelected}`)
+
+
+            if (countVegNotSelected == 0) {
+                $('#SelectAllVegetarian').prop('checked', true);
             }
         }
 
